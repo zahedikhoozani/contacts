0ae1a66fc8fbc589e4dafe7b05d507984d084410
karim zahedi <zahedikhoozani@gmail.com>
2018-10-07 17:10:23 +0330

    all in one page
    all in one page without search

--- delete.php
+++ delete.php
@@ -10,22 +10,32 @@
 	<?php
 	require_once( 'config.php' );
 
-	$phone = $_POST[ "phonenum" ];
+    $name = $_POST['search'];
 
-	// sql to delete a record
-	$sql = "DELETE FROM data WHERE phone=$phone";
+    $con= new mysqli("localhost","root","","Employee");
+    //$query = "SELECT * FROM employees
+   // WHERE first_name LIKE '%{$name}%' OR last_name LIKE '%{$name}%'";
 
-	if ( mysqli_query( $conn, $sql ) ) {
-		echo "<p>مخاطب مورد نظر با موفقیت حذف گردید</p>";
-	} else {
-		echo "Error deleting record: " . mysqli_error( $conn );
-	}
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
+$result = mysqli_query($con, "SELECT * FROM employees
+    WHERE first_name LIKE '%{$name}%' OR last_name LIKE '%{$name}%'");
+
+while ($row = mysqli_fetch_array($result))
+{
+        echo $row['first_name'] . " " . $row['last_name'];
+        echo "<br>";
+}
 
 	mysqli_close( $conn );
 	?>
 
 	<p align="center">
-		<input type="button" value="بازگشت" onclick="window.location.href='index.php'"/>
+		<input type="button" value="بازگشت" onkeydown="window.location.href='index.php'"/>
 	</p>
 
 </body>
--- index.php
+++ index.php
@@ -1,18 +1,76 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>دفتر تلفن</title>
-		<link rel="stylesheet" href="style1.css" type="text/css">
-	</head>
-    <body>
+<html dir="rtl">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>دفتر تلفن</title>
+	<link rel="stylesheet" href="style1.css" type="text/css">
+</head>
+
+<body>
 	<h1 align="center">دفتر تلفن</h1>
-		
-		<form>
-			<p align="center"><input type="button" value="مخاطب جدید" onclick="window.location.href='form.php'" /></p>
-			<p align="center"><input type="button" value="لیست مخاطبین" onclick="window.location.href='dbread.php'" /></p>
-			<p align="center"><input type="button" value="جستجو" onclick="window.location.href='delete.php'" /></p>
-		</form>
-		
-    </body>
+	<?php
+
+	function viewrecords() {
+		require( 'config.php' );
+		$sql = "SELECT fname, lname, phone FROM data";
+		mysqli_query( $conn, 'set names "utf8"' );
+		$result = mysqli_query( $conn, $sql );
+
+		echo '<form action="' . $_SERVER[ 'PHP_SELF' ] . '" method="post">
+
+		<table align="center" style="width:50%" border="1">
+			<tr>
+				<th>انتخاب</th>
+				<th>نام</th>
+				<th>نام خانوادگی</th>
+				<th>شماره تلفن</th>
+			</tr>';
+		if ( mysqli_num_rows( $result ) > 0 ) {
+			// output data of each row
+			$td1 = "<td align='center'>";
+			$td2 = "</td>";
+			while ( $row = mysqli_fetch_assoc( $result ) ) {
+				echo "<tr>";
+				echo $td1 . "<input type='checkbox' name='phonenum' value=" . $row[ "phone" ] . ">" . $td2;
+				echo $td1 . $row[ "fname" ] . $td2;
+				echo $td1 . $row[ "lname" ] . $td2;
+				echo "<td align='center' id=" . $row[ "phone" ] . ">" . $row[ "phone" ] . $td2;
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "0 results";
+		}
+		mysqli_close( $conn );
+	}
+
+
+	function deleterecord( $phone ) {
+		require( 'config.php' );
+		// sql to delete a record
+		$sql = "DELETE FROM data WHERE phone=$phone";
+
+		if ( mysqli_query( $conn, $sql ) ) {
+			echo "<script>alert ('مخاطب مورد نظر با موفقیت حذف گردید');</script>";
+		} else {
+			echo "Error deleting record: " . mysqli_error( $conn );
+		}
+		mysqli_close( $conn );
+	}
+
+
+	if ( $_SERVER[ "REQUEST_METHOD" ] == "POST" ) {
+		// collect value of input field
+		$phone = $_POST[ 'phonenum' ];
+		deleterecord( $phone );
+	}
+	viewrecords();
+	?>
+	<p align="center">
+		<input type="submit" value="حذف"/>
+		<input type="button" value="مخاطب جدید" onclick="window.location.href='form.php'"/>
+		<input type="text" value="جستجو" onkeydown="window.location.href='delete.php'"/>
+	</p>
+	</form>
+</body>
 </html>
